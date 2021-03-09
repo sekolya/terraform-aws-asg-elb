e62019b91eb2cbaea7f921e7408fb35e6695370e
Sekolya <dseil0308@gmail.com>
2021-03-09 18:45:57 +0000

    added elb

--- aws-asg-elb/elb.tf
+++ aws-asg-elb/elb.tf
@@ -1,18 +1,12 @@
 resource "aws_elb" "bar" {
   name               = "foobar-terraform-elb"
   availability_zones = data.aws_availability_zones.all.names
-
-
-
   listener {
     instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
   }
-
- 
-
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -20,8 +14,6 @@ resource "aws_elb" "bar" {
     target              = "HTTP:80/"
     interval            = 30
   }
-
-
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
@@ -31,7 +23,6 @@ resource "aws_elb" "bar" {
     Name = "foobar-terraform-elb"
   }
 }
-
 resource "aws_autoscaling_attachment" "asg_attachment_bar" {
   autoscaling_group_name = aws_autoscaling_group.example.id
   elb                    = aws_elb.bar.id
