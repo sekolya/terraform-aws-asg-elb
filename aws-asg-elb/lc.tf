51431d633d8b817e2f968f995e4c5b73d4fd9529
Sekolya <dseil0308@gmail.com>
2021-03-09 18:49:23 +0000

    added user data

--- aws-asg-elb/lc.tf
+++ aws-asg-elb/lc.tf
@@ -3,4 +3,5 @@ resource "aws_launch_template" "example" {
   image_id      = data.aws_ami.image.id
   instance_type = "c5.large"
   key_name = aws_key_pair.deployer.key_name
+  user_data = filebase64("${path.module}/userdata.sh")
 }
