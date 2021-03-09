239cb1ae23f886cb203df06811249222debecced
Sekolya <dseil0308@gmail.com>
2021-03-09 22:37:39 +0000

    added private subnet

--- class_vpc/private_subnet.tf
+++ class_vpc/private_subnet.tf
@@ -9,3 +9,24 @@ resource "aws_nat_gateway" "gw" {
 
   tags = var.tags
 }
+resource "aws_subnet" "private1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_cidr1
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags                    = var.tags
+}
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_cidr2
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+  tags                    = var.tags
+}
+resource "aws_subnet" "private3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_cidr3
+  availability_zone       = data.aws_availability_zones.available.names[2]
+  map_public_ip_on_launch = true
+  tags                    = var.tags
+}
