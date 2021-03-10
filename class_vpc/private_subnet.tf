d63ab124ca22609c271de5ddfd11b50a67db8b31
Sekolya <dseil0308@gmail.com>
2021-03-10 21:18:35 +0000

    added private rt

--- class_vpc/private_subnet.tf
+++ class_vpc/private_subnet.tf
@@ -30,3 +30,11 @@ resource "aws_subnet" "private3" {
   map_public_ip_on_launch = true
   tags                    = var.tags
 }
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw.id
+  }
+  tags = var.tags
+}
