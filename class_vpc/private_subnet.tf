3dd14ad3984b3820ab3579a6e7f7658ae30127e1
Sekolya <dseil0308@gmail.com>
2021-03-10 22:26:52 +0000

    added depends_on

--- class_vpc/private_subnet.tf
+++ class_vpc/private_subnet.tf
@@ -5,6 +5,7 @@ resource "aws_eip" "nat" {
 }
 #Creating NATGW and association with ElasticIP/location of NATGW in public subnet1
 resource "aws_nat_gateway" "gw" {
+  depends_on = [ aws_internet_gateway.gw ]
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public1.id
 
--- class_vpc/provider.tf
+++ class_vpc/provider.tf
@@ -5,6 +5,4 @@ provider "aws" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
-output "AZ" {
-  value = data.aws_availability_zones.available.names
-}
+
