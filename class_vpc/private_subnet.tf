ff99c69867c3b8485f3888b5d7a1b2960047651e
Sekolya <dseil0308@gmail.com>
2021-03-10 21:23:08 +0000

    association private subnet to rt

--- class_vpc/private_subnet.tf
+++ class_vpc/private_subnet.tf
@@ -38,3 +38,15 @@ resource "aws_route_table" "private" {
   }
   tags = var.tags
 }
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.private.id
+}
